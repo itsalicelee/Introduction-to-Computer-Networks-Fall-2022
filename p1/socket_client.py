8de15f63a8f93261b43b8f4b9f768c6e007f42b0
Alice <alice668149@gmail.com>
2022-10-13 14:02:10 +0800

    add p1

--- p1/socket_client.py
+++ p1/socket_client.py
@@ -1,14 +1,17 @@
 import socket
 import time
 
-with open('./client_log.txt', 'w') as logFile:
-    logFile.write("The Client is running..\n")
+with open('./b07303024_p1_client_result.log', 'w') as logFile:
+    # logFile.write("The Client is running..\n")
     logFile.flush()
 
     # Configure the server IP with its corrosponding port number
     # Specify the TCP connection type and make connection to the server
     # TODO start
-    HOST, PORT =
+    # HOST, PORT = "127.0.0.1", 4047
+    HOST, PORT = "140.112.42.104", 7777
+    client_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_s.connect((HOST, PORT))
     # TODO end
 
     Testcase = open('./p1_testcase', 'r')
@@ -17,11 +20,13 @@ with open('./client_log.txt', 'w') as logFile:
 
     # Write the information of HOST and PORT to the client_log.txt
     # TODO start
+    logFile.write("Connect to {}, using port number {}\n".format(HOST, PORT))
     # TODO end
 
     # Read test cases from p1_testcase
     # You can change the test case or create other test cases on your own
     for PreprossingLine in TestcaseContents:
+        
         Line = PreprossingLine.strip()
 
         # For connection stability
@@ -29,10 +34,15 @@ with open('./client_log.txt', 'w') as logFile:
 
         # Client sent the request to the server and receive the response from the server
         # TODO start
+        client_s.send(Line.encode())
+        msg = client_s.recv(1024)
+        logFile.write(msg.decode() + "\n")
         # TODO end
 
 
     # Close the socket
     # TODO start
+    logFile.write("N")
+    client_s.close()
     # TODO end
 logFile.close()
--- p1/socket_server.py
+++ p1/socket_server.py
@@ -1,17 +1,36 @@
 import socket
 from datetime import datetime
 
+def calculate(s):
+    if '*' in s:
+        s = s.split('*')
+        return float(s[0])*float(s[1])
+    elif '/' in s:
+        s = s.split('/')
+        return float(s[0])/float(s[1])
+    elif '+' in s:
+        s = s.split('+')
+        return float(s[0])+float(s[1])
+    elif '-' in s:
+        s = s.split('-')
+        return float(s[0])-float(s[1])
+
+
 with open('./server_log.txt', 'w') as logFile:
     # Specify the IP address and port number
     # (use "127.0.0.1" for localhost on local machine)
     # Create a socket and bind the socket to the address
     # TODO start
-    HOST, PORT =
+    # HOST, PORT = "127.0.0.1", 4047
+    HOST, PORT = "140.112.42.104", 7777
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
     # TODO end
 
     while True:
         # Listen to any request
         # TODO start
+        s.listen(0)
         # TODO end
 
         now = datetime.now()
@@ -23,20 +42,34 @@ with open('./server_log.txt', 'w') as logFile:
             try:
                 # Accept a new request
                 # TODO start
-                Client, Addr =
+                Client, Addr = s.accept()
                 # TODO end
 
                 while True:
-                    Client.send(b"Please input a question for calculation")
+                    Client.send(b"Received the message from server:\nPlease input a question for calculation")
                     # Recieve the data from the client, and send the answer back to the client
                     # Ask if the client want to terminate the process
                     # Terminate the process or continue
                     # TODO start
-                    # TODO end
+                    message = Client.recv(1000).decode()
+                    Client.send(("Question: " + message + "\n").encode())
+                    # logFile.write("Question: " + message + "\n")
+                    result = calculate(message)
+                    Client.send(("Get the answer from server: " + str(result) + "\n").encode())
+                    Client.send("Do you wish to continue? (Y/N)".encode())
+                    message = Client.recv(1000).decode()
+                    Client.send((message + "\n").encode())
+                    logFile.write(message + "\n")
+                    if(message == 'N'):
+                        Client.close()
+                        break    
+                break
+                # TODO end
             except ValueError:
                 continue
         break
     logFile.close()
     # Close the socket
     # TODO start
+    s.close()
     # TODO end
