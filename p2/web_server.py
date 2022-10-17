05f498083b58f8d555b870d67efe0bf43eceea9a
Alice <alice668149@gmail.com>
2022-10-17 14:00:10 +0800

    p2 done

--- p2/web_server.py
+++ p2/web_server.py
@@ -1,10 +1,16 @@
 from socket import *
 
 # Note: After finishing the program, try to type http://HOST:PORT/index.html in your browser for test
+# http://127.0.0.1:4047/index.html
+# http://127.0.0.1:4046/index.html
 
 ServerSocket = socket(AF_INET, SOCK_STREAM)
 # Create a socket and bind the socket to the address
 # TODO start
+HOST = "127.0.0.1"
+PORT = 4046
+ServerSocket.bind((HOST, PORT))
+ServerSocket.listen(0)
 # TODO end
 
 while True:
@@ -12,13 +18,15 @@ while True:
 
     # Establish the connection
     # TODO start
-    ConnectionSocket, Addr =
+    ConnectionSocket, Addr = ServerSocket.accept()
+    print(str(Addr) + "connected")
     # TODO end
 
     try:
         # Receive a HTTP request from the client
         # TODO start
-        RecvMessage =
+        RecvMessage = ConnectionSocket.recv(4096).decode()
+        print("RecvMsg: ", RecvMessage)
         # TODO end
 
         if RecvMessage == "":
@@ -31,16 +39,20 @@ while True:
         # Read data from the file that the client requested
         # Split the data into lines for further transmission
         # TODO start
-        DataInFile =
+        DataInFile = f.read()
         # TODO end
 
         # Send one HTTP header line into socket
         # Send HTTP Status to the client
         # TODO start
+        header = b'HTTP/1.1 200 OK\r\n'
+        ConnectionSocket.send(header)
         # TODO end
 
         # Send the Content Type to the client
         # TODO start
+        contentType = b'Content-Type: text/html; charset=UTF-8\n\n'
+        ConnectionSocket.send(contentType)
         # TODO end
 
         # Send the content of the requested file to the client
@@ -52,9 +64,15 @@ while True:
     except IOError:
         # Send the response message if the file is not found
         # TODO start
+        print("404 Not Found")
+        ConnectionSocket.send(b'HTTP/1.1 404 Not Found\r\n')
+        ConnectionSocket.send(b'Content-Type: text/html; charset=utf-8\r\n')
+        ConnectionSocket.send(b'404 Not Found\r\n')
         # TODO end
 
         # Close client socket
         # TODO start
+        ConnectionSocket.close()
         # TODO end
+ServerSocket.close()
 
